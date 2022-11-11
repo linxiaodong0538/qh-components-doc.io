3a5a639bcbc0aaeb57433efeea9eeea7b855a642
@zhuyadexiatian <734127061@qq.com>
2022-11-11 18:44:25 +0800

    update

---
+++ assets/guide_installation.md.49ac5dd8.js
@@ -0,0 +1,23 @@
+import{_ as s,o as n,c as a,h as p}from"./app.a5b848ca.js";const D=JSON.parse('{"title":"\u5B89\u88C5","description":"","frontmatter":{"realPath":"docs/guide/installation.md"},"headers":[],"relativePath":"guide/installation.md","lastUpdated":1663212290000}'),l={name:"guide/installation.md"},e=p(`<h1 id="\u5B89\u88C5" tabindex="-1">\u5B89\u88C5 <a class="header-anchor" href="#\u5B89\u88C5" aria-hidden="true">#</a></h1><div class="language-js"><button title="Copy Code" class="copy"></button><span class="lang">js</span><pre class="shiki vp-code-dark"><code><span class="line"><span style="color:#C9D1D9;"># </span><span style="color:#79C0FF;">NPM</span></span>
+<span class="line"><span style="color:#C9D1D9;">$ npm install qh</span><span style="color:#FF7B72;">-</span><span style="color:#C9D1D9;">components </span><span style="color:#FF7B72;">--</span><span style="color:#C9D1D9;">save</span></span>
+<span class="line"></span>
+<span class="line"></span>
+<span class="line"><span style="color:#C9D1D9;"># Yarn</span></span>
+<span class="line"><span style="color:#C9D1D9;">$ yarn add qh</span><span style="color:#FF7B72;">-</span><span style="color:#C9D1D9;">components</span></span>
+<span class="line"></span>
+<span class="line"></span>
+<span class="line"><span style="color:#C9D1D9;"># pnpm</span></span>
+<span class="line"><span style="color:#C9D1D9;">$ pnpm install qh</span><span style="color:#FF7B72;">-</span><span style="color:#C9D1D9;">components</span></span>
+<span class="line"><span style="color:#A5D6FF;">\`</span></span>
+<span class="line"></span></code></pre><pre class="shiki vp-code-light"><code><span class="line"><span style="color:#24292F;"># </span><span style="color:#0550AE;">NPM</span></span>
+<span class="line"><span style="color:#24292F;">$ npm install qh</span><span style="color:#CF222E;">-</span><span style="color:#24292F;">components </span><span style="color:#CF222E;">--</span><span style="color:#24292F;">save</span></span>
+<span class="line"></span>
+<span class="line"></span>
+<span class="line"><span style="color:#24292F;"># Yarn</span></span>
+<span class="line"><span style="color:#24292F;">$ yarn add qh</span><span style="color:#CF222E;">-</span><span style="color:#24292F;">components</span></span>
+<span class="line"></span>
+<span class="line"></span>
+<span class="line"><span style="color:#24292F;"># pnpm</span></span>
+<span class="line"><span style="color:#24292F;">$ pnpm install qh</span><span style="color:#CF222E;">-</span><span style="color:#24292F;">components</span></span>
+<span class="line"><span style="color:#0A3069;">\`</span></span>
+<span class="line"></span></code></pre></div>`,2),o=[e];function c(t,r,i,d,y,_){return n(),a("div",null,o)}const h=s(l,[["render",c]]);export{D as __pageData,h as default};
